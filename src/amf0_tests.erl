2a95ebcdbd306c210ac1435cdb77a6d0a57028de
Ruslan Babayev <ruslan@babayev.com>
2009-12-30 15:08:39 -0800

    Fix AMF0 unit tests.

--- src/amf0_tests.erl
+++ src/amf0_tests.erl
@@ -10,14 +10,20 @@
 %% Assert that a value encodes as specified.
 -define(_assertEncode(Encoding, Value),
 	?_assertMatch(Encoding, amf0:encode(Value))).
+-define(assertEncode(Encoding, Value),
+	?assertMatch(Encoding, amf0:encode(Value))).
 
 %% Assert that a binary decodes as specified.
 -define(_assertDecode(Value, Encoded),
 	?_assertMatch({Value,<<>>}, amf0:decode(Encoded))).
+-define(assertDecode(Value, Encoded),
+	?assertMatch({Value,<<>>}, amf0:decode(Encoded))).
 
 %% Assert that an encoded value decodes back to itself.
 -define(_assertCodec(E),
 	?LET(V, E, ?_assertEqual(V, element(1, amf0:decode(amf0:encode(V)))))).
+-define(assertCodec(E),
+	?LET(V, E, ?assertEqual(V, element(1, amf0:decode(amf0:encode(V)))))).
 
 encode_numbers_test_() ->
     [?_assertEncode(<<0,0.0:64/big-float>>, 0),
@@ -31,29 +37,29 @@ encode_booleans_test_() ->
      ?_assertEncode(<<1,1>>, true)].
 
 encode_null_test() ->
-    ?_assertEncode(<<5>>, null).
+    ?assertEncode(<<5>>, null).
 
 encode_undefined_test() ->
-    ?_assertEncode(<<6>>, undefined).
+    ?assertEncode(<<6>>, undefined).
 
 encode_unsupported_test() ->
-    ?_assertEncode(<<13>>, unsupported).
+    ?assertEncode(<<13>>, unsupported).
 
-encode_strings_test() ->
+encode_strings_test_() ->
     [?_assertEncode(<<2,0,6,"abcdef">>, <<"abcdef">>),
      ?_assertEncode(<<2,0,0>>,<<"">>),
      ?_assertEncode(<<2,0,4,"test">>, <<"test">>)].
 
 encode_long_string_test() ->
-    ?_assertEncode(<<12,0,1,165,230,"abcdef",_:108000/binary>>,
-		   list_to_binary(string:copies(<<"abcdef">>,18001))).
+    ?assertEncode(<<12,0,1,165,230,"abcdef",_:108000/binary>>,
+		  list_to_binary(string:copies("abcdef", 18001))).
 
 encode_array_test() ->
-    ?_assertEncode(<<10,0,0,0,1,0,64,88,64,0,0,0,0,0>>, "a").
+    ?assertEncode(<<10,0,0,0,1,0,64,88,64,0,0,0,0,0>>, "a").
 
 encode_object_test() ->
-    ?_assertEncode(<<3,0,3,"foo",2,0,3,"abc",0,3,"bar",1,1,0,0,9>>,
-		   {object,[{foo,<<"abc">>},{bar,true}]}).
+    ?assertEncode(<<3,0,3,"foo",2,0,3,"abc",0,3,"bar",1,1,0,0,9>>,
+		  {object,[{foo,<<"abc">>},{bar,true}]}).
 
 encode_arrays_test_() ->
     [?_assertEncode(<<10,0,0,0,0>>, []),
@@ -67,31 +73,31 @@ encode_dates_test_() ->
 		    {date,1252627967.442,0})].
 
 encode_xmldoc_test() ->
-    ?_assertEncode(<<15,0,0,0,6,"<xml/>">>, {xmldoc,"<xml/>"}).
+    ?assertEncode(<<15,0,0,0,6,"<xml/>">>, {xmldoc,<<"<xml/>">>}).
 
 encode_typed_object_test() ->
-    ?_assertEncode(<<16,0,5,"class",0,1,"a",1,0,0,2,"ab",1,1,0,0,9>>,
-		   {object,<<"class">>,[{a, false}, {ab, true}]}).
+    ?assertEncode(<<16,0,5,"class",0,1,"a",1,0,0,2,"ab",1,1,0,0,9>>,
+		  {object,<<"class">>,[{a, false}, {ab, true}]}).
 
 encode_avmplus_test() ->
-    ?_assertEncode(<<17,6,(8 bsl 1 bor 1),"testtest">>,
-		   {avmplus, <<"testtest">>}).
+    ?assertEncode(<<17,6,(8 bsl 1 bor 1),"testtest">>,
+		  {avmplus,<<"testtest">>}).
 
 decode_number_test() ->
-    ?_assertDecode(0.0, <<0,0.0:64/big-float>>).
+    ?assertDecode(0.0, <<0,0.0:64/big-float>>).
 
 decode_string_test() ->
-    ?_assertDecode(<<"abcdef">>, <<2,0,6,"abcdef">>).
+    ?assertDecode(<<"abcdef">>, <<2,0,6,"abcdef">>).
 
 decode_booleans_test_() ->
-    [?_assertDecode(false, <<1,0>>),
-     ?_assertDecode(true, <<1,1>>)].
+    [?_assertDecode(false,<<1,0>>),
+     ?_assertDecode(true,<<1,1>>)].
 
 decode_avmplus_test() ->
-    ?_assertDecode(1234, <<17,4,137,82>>).
+    ?assertDecode({avmplus,1234}, <<17,4,137,82>>).
 
 decode_references_test() ->
-    ?_assertDecode([{object,[]},{object,[]}], <<10,0,0,0,2,3,0,0,9,7,0,1>>).
+    ?assertDecode([{object,[]},{object,[]}], <<10,0,0,0,2,3,0,0,9,7,0,1>>).
 
 codec_numbers_test_() ->
     [?_assertCodec(0.0),
@@ -105,22 +111,22 @@ codec_booleans_test_() ->
      ?_assertCodec(true)].
 
 codec_null_test() ->
-    ?_assertCodec(null).
+    ?assertCodec(null).
 
 codec_undefined_test() ->
-    ?_assertCodec(undefined).
+    ?assertCodec(undefined).
 
 codec_unsupported_test() ->
-    ?_assertCodec(unsupported).
+    ?assertCodec(unsupported).
 
 codec_string_test() ->
-    ?_assertCodec(<<"quickfox">>).
+    ?assertCodec(<<"quickfox">>).
 
 codec_long_string_test() ->
-    ?_assertCodec(list_to_binary(string:copies("lazydog", 18000))).
+    ?assertCodec(list_to_binary(string:copies("lazydog", 18000))).
 
 codec_object_test() ->
-    ?_assertCodec({object,[{abc,5.5},{def,false},{xyz,null}]}).
+    ?assertCodec({object,[{abc,5.5},{def,false},{xyz,null}]}).
 
 codec_arrays_test_() ->
     [?_assertCodec([]),
@@ -132,12 +138,15 @@ codec_dates_test_() ->
      ?_assertCodec({date,5000.0,0})].
 
 codec_xmldoc_test() ->
-    ?_assertCodec({xmldoc,<<"<xml></xml>">>}).
+    ?assertCodec({xmldoc,<<"<xml></xml>">>}).
 
 codec_typed_object_test() ->
-    ?_assertCodec({object,<<"class">>,[{m1,true},{m2,false}]}).
+    ?assertCodec({object,<<"class">>,[{m1,true},{m2,false}]}).
+
+codec_avmplus_test() ->
+    ?assertCodec({avmplus,123}).
 
 codec_nested_array_of_dates_test() ->
     D1 = {date,789456.21,0},
     D2 = {date,123456.45,0},
-    ?_assertCodec([[D2,D2],D1,[D2,D2],[D1,D2],[D2,[D1,D2],D1],D2,D1]).
+    ?assertCodec([[D2,D2],D1,[D2,D2],[D1,D2],[D2,[D1,D2],D1],D2,D1]).
